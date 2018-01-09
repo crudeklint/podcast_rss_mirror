f6ee080d78c3520f462c2ff7b35d4f572a8333a3
Christoffer Rudeklint <c.rudeklint@gmail.com>
2018-01-10 00:51:10 +0100

    fixed faulty parsing of rss

--- podcast_rss_mirror.py
+++ podcast_rss_mirror.py
@@ -1,11 +1,13 @@
 import os
+import io
 import sys
 import time
-import datetime
 import argparse
+import urllib.request
 
 # import wget
-import urllib.request
+from datetime import datetime
+
 
 from subprocess import call
 from xml.etree import ElementTree as ET
@@ -14,6 +16,7 @@ parser = argparse.ArgumentParser( description="This script creates a rss-podcast
 parser.add_argument( "-i", "--input_href", help="Input podcast rss address", required=True) 
 parser.add_argument( "-p", "--podcast_name", help="Local pocast name", required=True, default=None ) 
 parser.add_argument( "-n", "--new_href", help="New base-href", required=True, default=None) 
+parser.add_argument( "--DOWNLOAD_ALL", help="Download all podcasts (default 1 year)", required=False, action='store_true' ) 
 parser.add_argument( "--TEST", help="If this is used, only 10 files will be downloaded", required=False, action='store_true' ) 
 
 args = vars( parser.parse_args( ) ) 
@@ -22,11 +25,29 @@ newhref = args["new_href"]
 pod_name = args["podcast_name"]
 pod_real_href = args["input_href"]
 test_mode = args["TEST"]
+download_all = args["DOWNLOAD_ALL"]
 
 time_threshold = 60*60*24-100
+oldest_pod = 365 #days
+
+def uglyfix( input_path ) :	
+	thefile = io.open( input_path, mode="r", encoding="utf-8")
+	file_string = thefile.read()
+	thefile.close()
+	
+	for tag in ["itunes:summary", "description", "title", "itunes:keywords", "itunes:subtitle"] :
+		fixed_tag = "<" + tag + ">"
+		fixed_endtag = "</" + tag + ">"
+	
+		file_string = file_string.replace( fixed_tag, fixed_tag+"<![CDATA[" ).replace( fixed_endtag, "]]>"+fixed_endtag )
+	
+	thefile = open( input_path, "w" )
+	thefile.write( file_string )
+	thefile.close()
+	
 
 def logmess( message, log_file_obj, lastlog = False ) :
-	current_timestamp = datetime.datetime.now().replace(microsecond=0).isoformat(" ")
+	current_timestamp = datetime.now().replace(microsecond=0).isoformat(" ")
 	
 	if( lastlog ) :
 		eol = "\n\n"
@@ -45,6 +66,8 @@ def download_file( input, output ) :
 		
 def create_pod_mirror( rss_href, podname, new_base_href ) :
 
+	delete_list = []
+
 	now_time = time.time()
 	script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
 	log_path = os.path.join( script_dir, "podcast_mirror.log" )
@@ -55,15 +78,21 @@ def create_pod_mirror( rss_href, podname, new_base_href ) :
 	
 	local_pod_dir = os.path.join( script_dir, pod_name )
 	local_pod_rss = os.path.join( local_pod_dir, pod_name + ".rss" )
-
+	pod_last_download_path = os.path.join( local_pod_dir, "last_download.log" )
+	
 	if( not os.path.exists( local_pod_dir ) ) :
 		os.mkdir( local_pod_dir )
 
-	if( os.path.exists( local_pod_rss ) ):
-		local_date_node = ET.parse( local_pod_rss ).getroot().findall( "last_download" )
+	if( os.path.exists( pod_last_download_path ) ):
+		last_download_file = open( pod_last_download_path )
+		last_download_string = last_download_file.read()
 		
-		if( len( local_date_node ) > 0 ) :
-			last_download = int( local_date_node[0].text )
+		if( last_download_string == "" ) :
+			last_download = 0
+		else :
+			last_download = int( last_download_string )
+		
+		last_download_file.close()
 		
 		if( ( now_time - last_download < time_threshold ) and not test_mode ) :
 			logmess( "Download timeout has not been reached! Exiting", log_file, True  )
@@ -71,14 +100,20 @@ def create_pod_mirror( rss_href, podname, new_base_href ) :
 	
 	tmp_podcast_rss = os.path.join( script_dir, pod_name + "_rss.tmp" )
 	
+	ET.register_namespace("itunes", "http://www.itunes.com/dtds/podcast-1.0.dtd" )
+	ET.register_namespace("atom", "http://www.w3.org/2005/Atom" )
+	ET.register_namespace("sr", "http://www.sverigesradio.se/podrss" )
+	
 	download_file( pod_real_href, tmp_podcast_rss )
 	rss_tree = ET.parse( tmp_podcast_rss )
 	os.remove( tmp_podcast_rss )
-	
+
 	root_elem = rss_tree.getroot()
 	data_node = root_elem.findall( 'channel' )
-
+	
 	i=0
+	
+	do_log = True
 	
 	for child in data_node[0]:
 		if( child.tag != "item" ) :
@@ -87,32 +122,61 @@ def create_pod_mirror( rss_href, podname, new_base_href ) :
 		i+=1
 		
 		if( i > 10 and test_mode ) :
-			logmess( "Test mode, exiting.", log_file, True  )
-			return 0
+			delete_list.append( child )
+			if( do_log ) : 
+				logmess( "Test mode, exiting.", log_file )
+				
+			do_log = False
+			continue
 		
-		mp3link = child.findall( "guid" )[0]
-		oldlink = mp3link.text
+		# mp3link = child.findall( "guid" )[0]
+		# oldlink = mp3link.text
 		
+		pub_date = child.findall( "pubDate" )[0]
+		datestring = pub_date.text
+		
+		date_obj = datetime.strptime(datestring, "%a, %d %b %Y %H:%M:%S %Z")
+		time_delta = datetime.now() - date_obj
+
+		mp3link = child.findall( "enclosure" )[0]
+		oldlink = mp3link.get( "url" )
 		link_basename = os.path.basename( oldlink )
+		
+		if( time_delta.days > oldest_pod and not download_all ) :
+			delete_list.append( child )
+			logmess( link_basename + " too old. deleting from rss" , log_file )
+			continue
+		
+		# title = child.findall( "title" )[0]
+		# title.text = "<![CDATA[" + title.text + "]]>"
 		
 		newlink = os.path.join( newhref, pod_name, link_basename )
 		local_path = os.path.join( local_pod_dir, link_basename )
 		
-		mp3link.text = newlink
-		
+		# mp3link.text = newlink
+		mp3link.set( "url", newlink )
+	
 		if( not os.path.isfile( local_path ) ) :
-			download_file( oldlink, local_path )  
+			# download_file( oldlink, local_path )  
 			logmess( "downloading " + link_basename, log_file )
 			time.sleep( 1 )
 		else :
 			logmess( "skipping " + link_basename + ", already exists", log_file )
 
-	date_node = ET.SubElement( root_elem, "last_download" )
-	date_node.text = str( int( now_time ) )
-	rss_tree.write( local_pod_rss )
+	for deletechild in delete_list:
+		data_node[0].remove( deletechild )
+			
+	last_download_file = open( pod_last_download_path, "w" )
+	last_download_file.write( str( int( now_time ) ) )
+	last_download_file.close()
+	
+	rss_tree.write( local_pod_rss, encoding="UTF-8", xml_declaration=True )
+	
+	uglyfix( local_pod_rss )
 	logmess( "Finished mirroring " + rss_href, log_file, True )
 
 create_pod_mirror( pod_real_href, pod_name, newhref )
+
 
 
 	
